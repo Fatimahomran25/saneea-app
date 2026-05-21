f107b61f7256c36a7d04bfcf5360c90782711152
Manar Alrazin <ma.alrazin@gmail.com>
2026-05-21 18:57:30 +0300

    444 is done

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
   static const String emulatorBaseUrl = 'http://10.0.2.2:5001';
 
   // Laptop Wi-Fi IPv4 from ipconfig for real phone testing on the same network.
-  static const String phoneBaseUrl = 'http://192.168.8.5:5001';
+  static const String phoneBaseUrl = 'http://10.6.192.57:5001';
 
   // Switch this to emulatorBaseUrl when testing on the Android emulator.
   static const String baseUrl = phoneBaseUrl;
